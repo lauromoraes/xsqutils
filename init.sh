8dd0a609fe2484ef5dc720597ac987fd34b7981c
Lauro Moraes <lauromoraes@ufop.edu.br>
2025-01-28 19:40:03 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,7 +18,16 @@ fi
 
 . env/bin/activate
 
-# pip install numpy
-# pip install cython
-# pip install numexpr
-# pip install tables==2.3.1
+pip install numpy==1.9.2
+pip install cython==0.22
+pip install numexpr==2.4.4
+
+pip install --upgrade --force-reinstall numpy==1.9.2
+
+# Set environment variables to help PyTables find HDF5
+HDF5_DIR=/usr/lib/x86_64-linux-gnu/hdf5/serial
+CFLAGS="-I/usr/include/hdf5/serial"
+LDFLAGS="-L/usr/lib/x86_64-linux-gnu/hdf5/serial"
+
+# Install PyTables with the correct HDF5 paths
+pip install --no-cache-dir tables==2.3.1 --global-option=build_ext
